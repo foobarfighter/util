1be80d7969fdb231a0ef5013ccfa21bbd1ad6918
Bob Remeika <bob.remeika@gmail.com>
2009-11-08 03:20:09 -0800

    Updated build profile

--- buildscripts/profiles/rails.profile.js
+++ buildscripts/profiles/rails.profile.js
@@ -3,7 +3,16 @@ dependencies = {
 		{
 			name: "../dojox/rails.js",
 			dependencies: [
-				"dojox.rails"
+				"dojox.rails",
+				"dojox.rails.listeners",
+				"dojox.rails.decorators.common",
+				"dojox.rails.decorators.Request",
+				"dojox.rails.decorators.Updater",
+				"dojox.rails.decorators.Observer",
+				"dojox.rails.decorators.FieldObserver",
+				"dojox.rails.decorators.FormObserver",
+				"dojox.rails.decorators.RemoteForm",
+				"dojox.rails.decorators.RemoteLink"
 			]
 		}
 	],
